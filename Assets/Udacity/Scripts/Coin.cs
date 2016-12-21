793481d74ed7c1bedcac5128f1182f82842c6c60
Reuben K. Coutinho <reuben.coutinho@gmail.com>
2016-12-21 16:59:31 +1000

    Chore: added 5 coins and created a coinpoof prefab
    The coinpoof prefab will contain a custom sound for the coin and will get assigned to the position of the current coin.

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -6,7 +6,8 @@ public class Coin : MonoBehaviour
 {
     //Create a reference to the CoinPoofPrefab
 
-    public void OnCoinClicked() {
+    public void OnCoinClicked(GameObject poof) {
+        poof.transform.position = gameObject.transform.position;
         // Instantiate the CoinPoof Prefab where this coin is located
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
