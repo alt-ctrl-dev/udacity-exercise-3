bbf0ad22867210609d6c68dfe2d6267228943cbe
Reuben Coutinho <reuben.coutinho@gmail.com>
2016-12-21 22:19:00 +1000

    Chore: Added a HUD to display coin and key status

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour 
+public class Coin : MonoBehaviour
 {
 	private static int coinCount;
 
-	void Start(){
-		print ("Coin count = "+coinCount);
+	void Start ()
+	{
+		print ("Coin count = " + coinCount);
 	}
 
-    //Create a reference to the CoinPoofPrefab
-    public void OnCoinClicked(GameObject poof) {
+	//Create a reference to the CoinPoofPrefab
+	public void OnCoinClicked (GameObject poof)
+	{
 		// Instantiate the CoinPoof Prefab where this coin is located
 		// Make sure the poof animates vertically
-		Object.Instantiate(poof, gameObject.transform.position, Quaternion.Euler(Camera.main.transform.rotation.eulerAngles));
+		Object.Instantiate (poof, gameObject.transform.position, Quaternion.Euler (Camera.main.transform.rotation.eulerAngles));
 
 		// Destroy this coin. Check the Unity documentation on how to use Destroy
-		Destroy(gameObject);
+		Destroy (gameObject);
 		coinCount++;
-		print ("New Coin count = "+coinCount);
-    }
+		print ("New Coin count = " + coinCount);
+		UpdateText ();
+	}
+
+	private void UpdateText ()
+	{
+		var textObj = GameObject.FindGameObjectWithTag ("CoinText").GetComponent<TextMesh>();
+		textObj.text = string.Format ("Coins {0}/5", coinCount);
+	}
 
 }
