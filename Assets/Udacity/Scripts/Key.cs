475d3232d70405aae5381256f211b97d0be3279f
Reuben K. Coutinho <reuben.coutinho@gmail.com>
2016-12-22 11:21:34 +1000

    Refactor: Updated Key.cs to open door

--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -8,12 +8,12 @@ public class Key : MonoBehaviour
     public GameObject keyPoof;
     public Door door;
 
-    private static int keyCount;
+    private static bool hasKey;
 
     void Start()
     {
-        keyCount = 0;
-        print("key count = " + keyCount);
+        hasKey = false;
+        print("key count = " + hasKey);
     }
 
     void Update()
@@ -21,23 +21,32 @@ public class Key : MonoBehaviour
 		//Bonus: Key Animation
 	}
 
-	public void OnKeyClicked()
-	{
-        // Instatiate the KeyPoof Prefab where this key is located
-        // Make sure the poof animates vertically
-        Object.Instantiate(keyPoof, gameObject.transform.position, Quaternion.Euler(Camera.main.transform.rotation.eulerAngles));
+    public void OnKeyClicked()
+    {
+        if (!hasKey)
+        {
+            // Instatiate the KeyPoof Prefab where this key is located
+            // Make sure the poof animates vertically
+            Object.Instantiate(keyPoof, gameObject.transform.position, Quaternion.Euler(Camera.main.transform.rotation.eulerAngles));
 
-        // Call the Unlock() method on the Door
+            // Call the Unlock() method on the Door
 
-        // Destroy the key. Check the Unity documentation on how to use Destroy
-        Destroy(gameObject);
-        UpdateText();
+            // Destroy the key. Check the Unity documentation on how to use Destroy
+            Destroy(gameObject);
+            hasKey = true;
+            UpdateText();
+        }
+        else
+        {
+            door.Unlock();
+            print("Unlocking door");
+        }
     }
 
     private void UpdateText()
     {
         var textObj = GameObject.FindGameObjectWithTag("KeyText").GetComponent<TextMesh>();
-        textObj.text = string.Format("Key {0}/1", keyCount);
+        textObj.text = string.Format("Key {0}/1", (hasKey?1:0));
     }
 
 }
